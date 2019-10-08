ff199272a9c2c8fe2e4597cd2a703042b64fe9d3
Trevor <tgrabham@sfu.ca>
2019-10-07 23:07:51 -0700

    finished the shell of index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var app = express();
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({connectionString: process.env.DB_URL});
+
+app.use(express.static.(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.get('/', (req,res) => {
+  res.render('tokimon');
+});
